2108a569b389bc51e9f1ffc5078fc2de6a40a389
Carlos Gochez <cgotcha@gmail.com>
2016-08-18 08:01:09 -0600

    Ejercicios de Banco y borrar PlazoFijo (tratando de elimianar el issue con ella)

--- src/herencia/Banco.java
+++ src/herencia/Banco.java
@@ -28,4 +28,43 @@ public class Banco {
     public void addCuenta(int num, String cli, String mo, TipoCuenta tipo){
         
     }
+    
+    /**
+     * Deposita un monto a una cuenta existente
+     * @param nume
+     * @param monto 
+     */
+    public void depositar(int num, double monto){
+        
+    }
+    
+    /**
+     * Retira de una cuenta existente
+     * @param num Numero de Cuenta
+     * @param monto Monto a Retirar
+     * @return True si pudo retirar o False si no
+     */
+    public boolean retirar(int num, double monto){
+        return false;
+    }
+    
+    /**
+     * Recorre todas las cuentas guardadas para
+     * registrarle los intereses
+     */
+    public void registrarIntereses(){
+        
+    }
+    
+    /**
+     * Busca una cuenta de Plazo Fijo, si existe
+     * se actualiza su plazo
+     * @param num Numero de Cuenta
+     * @param y Año
+     * @param m Mes (1-12)
+     * @param d dia
+     */
+    public void setPlazoDeCuentaFija(int num, int y, int m, int d){
+        
+    }
 }
